98666d8d0b4d9b6e93bcd461729807239a139958
vargas.rita <159588529+FV-MariaRita@users.noreply.github.com>
2025-04-28 18:17:51 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import { Link } from "@mui/material";
-import { GitHub } from "@mui/icons-material";
-import { Stack } from "@mui/system";
 import OlatcgNavbar from "../components/OlatcgNavbar";
 import AppRoutes from "../routes/AppRoutes";
 import { getMessage } from "../services/MessageService";
+import OlatcgFooter from "../components/OlatcgFooter";
 
 const App = () => {
   return <>
     <OlatcgNavbar />
     <AppRoutes />
-    <Stack
-      direction="row"
-      alignItems="center"
-      justifyContent="flex-end"
-      sx={{position: 'fixed', width: '100%', bottom: 0, backgroundColor: '#95AB6D', color: "#ffffff", padding: 0.3}}
-    > 
-      <GitHub />
-      <Link href="https://github.com/LuizMVB" underline="hover" sx={{color: "#ffffff", pr: 2}}><span style={{paddingLeft: 8}}>{getMessage('common.developedBy')}</span></Link>
-    </Stack>
+    <OlatcgFooter />    
   </>;
 }
 
